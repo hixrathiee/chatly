9793fc322fdf173da79c54a72d667e5e400ded4a
hixrathiee <161725960+hixrathiee@users.noreply.github.com>
2025-08-05 15:37:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import { app, server } from "./socket/socket.js"
 const port = process.env.PORT || 5000
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://realtimechatapp-qxrk.onrender.com",
     credentials:true
 }))
 app.use(express.json())
