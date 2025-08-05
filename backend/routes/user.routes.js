e53bb7e9d31bff83861c7edcfe418996742e8191
Anjali Rathi <anjirathi@gmail.com>
2025-08-05 15:23:48 +0530

    first commit

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,15 @@
+import express, { Router } from "express"
+import { editProfile, getCurrentUser, getOtherUsers, search } from "../controllers/user.controller.js"
+import isAuth from "../middlewares/isAuth.js"
+import { upload } from "../middlewares/multer.js"
+
+
+const userRouter = express.Router()
+
+
+userRouter.get("/current",isAuth,getCurrentUser)
+userRouter.get("/others",isAuth,getOtherUsers)
+userRouter.put("/profile",isAuth,upload.single("image"),editProfile)
+userRouter.get("/search",isAuth,search)
+
+export default userRouter
